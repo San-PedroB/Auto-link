46ee6ae6dd4e1b832a7ec591a7952c1ee8c269da
San_Pedro <pedro.beltran@gmail.com>
2024-09-08 22:48:38 -0300

    commit de respaldo

--- src/app/viajes-en-curso/viajes-en-curso.page.ts
+++ src/app/viajes-en-curso/viajes-en-curso.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { ServicioViajesService } from '../services/servicio-viajes.service'; // Importa el servicio de viajes
+import { RolUsuarioService } from '../services/rol-usuario.service'; // Servicio de rol de usuario
+import { ServicioViajesService } from '../services/servicio-viajes.service'; // Servicio de viajes
 import { ToastController } from '@ionic/angular'; // Para mostrar mensajes Toast
 
 @Component({
@@ -10,18 +11,27 @@ import { ToastController } from '@ionic/angular'; // Para mostrar mensajes Toast
 export class ViajesEnCursoPage implements OnInit {
 
   viaje: any = null;
+  esConductor: boolean = false;
+  esPasajero: boolean = false;
 
-  constructor(private servicioViajes: ServicioViajesService, private toastController: ToastController) { }
+  constructor(private servicioViajes: ServicioViajesService, private rolUsuarioService: RolUsuarioService, private toastController: ToastController) { }
 
   ngOnInit() {
     // Obtener los datos del viaje desde el servicio
     this.viaje = this.servicioViajes.getDatos();
+    console.log('Viaje:', this.viaje);
+
+    // Verificar si el usuario es conductor o pasajero
+    this.esConductor = this.rolUsuarioService.esConductor();
+    this.esPasajero = this.rolUsuarioService.esPasajero();
+    console.log('esConductor:', this.esConductor);  // Verificar el rol de conductor
+    console.log('esPasajero:', this.esPasajero);  // Verificar el rol de pasajero
   }
 
   // Función para terminar el viaje
   async terminarViaje() {
     // Terminar el viaje usando el servicio
-    this.servicioViajes.borrarViaje();  // No pasamos el formulario desde aquí
+    this.servicioViajes.borrarViaje();
 
     // Actualizar la vista para reflejar que no hay viaje en curso
     this.viaje = null;
@@ -31,6 +41,19 @@ export class ViajesEnCursoPage implements OnInit {
       message: 'El viaje ha sido terminado.',
       duration: 2000,
       position: 'bottom'
+    });
+    await toast.present();
+  }
+
+  // Función para tomar el viaje (solo para pasajeros)
+  async tomarViaje() {
+    const viajeActivo = true;
+    // Lógica para que el pasajero tome el viaje
+    const toast = await this.toastController.create({
+      message: 'Has tomado el viaje.',
+      duration: 2000,
+      position: 'bottom'
+
     });
     await toast.present();
   }
